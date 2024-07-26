619596fe42948e592939edbca00aaeb852a41cb2
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-07-27 06:36:14 +0900

    chore: GitHub Actions設定ファイルとユニットテストを実装した

---
+++ UnitTestByGitHubAction/UnitTestByGitHubActionTests/UnitTestByGitHubActionTests.swift
@@ -0,0 +1,13 @@
+//
+//  UnitTestByGitHubActionTests.swift
+//  UnitTestByGitHubActionTests
+//
+//  Created by Wataru Miyakoshi on 2024/07/27.
+//
+
+import Testing
+
+struct UnitTestByGitHubActionTests {
+    @Test func testExample() async throws {
+    }
+}
